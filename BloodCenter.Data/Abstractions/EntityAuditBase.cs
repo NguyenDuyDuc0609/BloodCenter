b2f26ba94e74480deda4253c0ed6adbe95ce645c
bgxinh <nguyenduyduc0609@gmail.com>
2025-02-26 18:56:00 +0700

    commit auditbase

---
+++ BloodCenter.Data/Abstractions/EntityAuditBase.cs
@@ -0,0 +1,18 @@
+﻿using BloodCenter.Data.Abstractions.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Data.Abstractions
+{
+    public abstract class EntityAuditBase<T> : IEntityAuditBase<T>
+    {
+        public T Id { get; set; }
+        public bool IsDelete { get ; set ; }
+        public DateTimeOffset DeleteDate { get ; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset ModifiedDate { get; set; }
+    }
+}
---
+++ BloodCenter.Data/Entities/Activity.cs
@@ -0,0 +1,14 @@
+﻿using BloodCenter.Data.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Data.Entities
+{
+    public class Activity : EntityAuditBase<Guid>
+    {
+
+    }
+}
